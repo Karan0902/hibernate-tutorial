07040fb3175020603f953ccaf2cff6fa2001d176
Karan Motiramani <karan0902@yahoo.co.in>
2023-01-26 21:00:18 +0530

    * query the db by creating your own SQL queries

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -32,9 +32,21 @@ public class QueryStudentDemo {
 			
 //			query students: lastName='Doe'
 			students = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
-			System.out.println("\nStudents who have last name of Doe");
+			System.out.println("\nStudents who have last name of Doe ===>");
 			displayStudents(students);
 			
+			
+//			query students: lastName='Doe' OR firstName='Daffy'
+			students = session.createQuery("from Student s where " + 
+											"s.lastName='Doe' OR s.firstName='Daffy'").getResultList();
+			System.out.println("\nStudents with lastName=Doe OR firstName=Daffy ===>");
+			displayStudents(students);
+			
+			
+//			query students: email ends LIKE %luv2code.com
+			students = session.createQuery("from Student s where s.email LIKE '%luv2code.com'").getResultList();
+			System.out.println("\nStudents with email ===>");
+			displayStudents(students);
 			
 //			commit the transaction
 			session.getTransaction().commit();
